5364da9d558ec5c0545494d33ecd7d671472e312
sd2899 <34939012+sd2899@users.noreply.github.com>
2024-09-17 15:48:01 +0530

    Update Glance-Config.sh

--- Glance-Config.sh
+++ Glance-Config.sh
@@ -19,14 +19,14 @@ else
 fi
 echo "Database setup completed"
 
-. admin-openrc
+. /home/openstack/admin-openrc
 # create service project
 openstack project create --domain default --description "Project description" service
 
 # Create the Glance User and assign the admin role
 openstack user create --domain default --password "g123" glance
 openstack role add --project service --user glance admin
-openstack service create --name glance --description "OpenStack Image" image
+SERVICE_ID=$(openstack service create --name glance --description "OpenStack Image" image -f value -c id)
 openstack role add --user glance --user-domain Default --system all reader
 
 # Create Glance endpoints
@@ -36,11 +36,10 @@ endpoint_id=$(openstack endpoint create --region RegionOne image admin http://co
 
 # install the package
 sudo apt install glance -y
-sleep 20
 
 # Create the OpenStack Service
-echo "Creating OpenStack service: $SERVICE_NAME..."
-SERVICE_ID=$(openstack service create --name "$SERVICE_NAME" --description "$SERVICE_DESCRIPTION" "$SERVICE_TYPE" -f value -c id)
+#echo "Creating OpenStack service: $SERVICE_NAME..."
+#SERVICE_ID=$(openstack service create --name "$SERVICE_NAME" --description "$SERVICE_DESCRIPTION" "$SERVICE_TYPE" -f value -c id)
 
 #configure the Glance Config file
 config_file="/etc/glance/glance-api.conf"
@@ -80,7 +79,7 @@ if [ -f "$config_file" ]; then
 		echo "Existing connection string found, commenting it out"
 		
 		# Comment out the existing connection string
-		sudo sed -i "/^\[database\]/a$NEW_DB_CONNECTION" "$config_file"
+		sudo sed -i "s/^.*connection = .*/# &/" "$config_file"
 		
 	    else
 		echo "No existing connection string found in the [database] section"
@@ -103,7 +102,19 @@ if [ -f "$config_file" ]; then
 	    echo "[keystone_authtoken] section found in $config_file"
 
 	    # Add the keystone_authtoken below the [keystone_authtoken] section
-	    sudo sed -i "/^\[keystone_authtoken\]/a$keystone_authtoken_config" "$config_file"
+	     while IFS= read -r line; do
+		key=$(echo "$line" | cut -d'=' -f1 | xargs)
+		value=$(echo "$line" | cut -d'=' -f2- | xargs)
+		
+		# Check if the key exists in the section, if not, add it
+		if grep -A10 "^\[keystone_authtoken\]" "$config_file" | grep -q "^$key ="; then
+		    echo "Updating $key in keystone_authtoken section"
+		    sudo sed -i "/^\[keystone_authtoken\]/,/^\[/ s|^$key =.*|$key = $value|" "$config_file"
+		else
+		    echo "Adding $key to keystone_authtoken section"
+		    sudo sed -i "/^\[keystone_authtoken\]/a$key = $value" "$config_file"
+		fi
+	    done <<< "$keystone_authtoken_config"
 	    echo "keystone_authtoken added to [keystone_authtoken] section"
 
 	else
@@ -111,7 +122,22 @@ if [ -f "$config_file" ]; then
 	    echo "Adding the [keystone_authtoken] section and new connection string"
 
     	# If the [keystone_authtoken] section doesn't exist, append it to the config file
-    	echo -e "\n[keystone_authtoken]\n$keystone_authtoken_config" | sudo tee -a "$config_file"
+    	#echo -e "\n[keystone_authtoken]\n$keystone_authtoken_config" | sudo tee -a "$config_file"
+    	echo "[keystone_authtoken]" >> "$config_file"
+    	
+    	# Add the keystone_authtoken below the [keystone_authtoken] section
+	while IFS= read -r line; do
+	   key=$(echo "$line" | cut -d'=' -f1 | xargs)
+	   value=$(echo "$line" | cut -d'=' -f2- | xargs)
+	   # Check if the key exists in the section, if not, add it
+	   if grep -A10 "^\[keystone_authtoken\]" "$config_file" | grep -q "^$key ="; then
+	       echo "Updating $key in keystone_authtoken section"
+	       sudo sed -i "/^\[keystone_authtoken\]/,/^\[/ s|^$key =.*|$key = $value|" "$config_file"
+	   else
+	       echo "Adding $key to keystone_authtoken section"
+	       sudo sed -i "/^\[keystone_authtoken\]/a$key = $value" "$config_file"
+	   fi
+	   done <<< "$keystone_authtoken_config"
 	fi
 	
 	# edit paste_deploy section
@@ -134,8 +160,20 @@ if [ -f "$config_file" ]; then
 	if grep -q "^\[glance_store\]" "$config_file"; then
 	    echo "[glance_store] section found in $config_file"
 
-	    # Add the paste_deploy below the [glance_store] section
-	    sudo sed -i "/^\[glance_store\]/a$glance_store_conf" "$config_file"
+	    # Add the glance_store below the [glance_store] section
+	    while IFS= read -r line; do
+		key=$(echo "$line" | cut -d'=' -f1 | xargs)
+		value=$(echo "$line" | cut -d'=' -f2- | xargs)
+		
+		# Check if the key exists in the section, if not, add it
+		if grep -A10 "^\[glance_store\]" "$config_file" | grep -q "^$key ="; then
+		    echo "Updating $key in glance_store section"
+		    sudo sed -i "/^\[glance_store\]/,/^\[/ s|^$key =.*|$key = $value|" "$config_file"
+		else
+		    echo "Adding $key to glance_store section"
+		    sudo sed -i "/^\[glance_store\]/a$key = $value" "$config_file"
+		fi
+	    done <<< "$glance_store_conf"
 	    echo "glance_store added to [glance_store] section"
 
 	else
@@ -143,7 +181,20 @@ if [ -f "$config_file" ]; then
 	    echo "Adding the [glance_store] section and new connection string"
 
     	# If the [glance_store] section doesn't exist, append it to the config file
-    	echo -e "\n[glance_store]\n$glance_store_conf" | sudo tee -a "$config_file"
+    	#echo -e "\n[glance_store]\n$glance_store_conf" | sudo tee -a "$config_file"
+    	echo "[glance_store]" >> "$config_file"
+    	while IFS= read -r line; do
+	   key=$(echo "$line" | cut -d'=' -f1 | xargs)
+	   value=$(echo "$line" | cut -d'=' -f2- | xargs)
+	   # Check if the key exists in the section, if not, add it
+	   if grep -A10 "^\[glance_store\]" "$config_file" | grep -q "^$key ="; then
+	       echo "Updating $key in glance_store section"
+	       sudo sed -i "/^\[glance_store\]/,/^\[/ s|^$key =.*|$key = $value|" "$config_file"
+	   else
+	       echo "Adding $key to glance_store section"
+	       sudo sed -i "/^\[glance_store\]/a$key = $value" "$config_file"
+	   fi
+	done <<< "$glance_store_conf"
 	fi
 	
 	#edit the oslo_limit section
@@ -162,16 +213,56 @@ if [ -f "$config_file" ]; then
 	    fi
 	    echo "Service created with ID: $endpoint_id"
 	    
-	    # Add the paste_deploy below the [oslo_limit] section
-	    sudo sed -i "/^\[oslo_limit\]/a$oslo_limit_conf" "$config_file"
-	    echo "glance_store added to [oslo_limit] section"
+	    # Add the oslo_limit below the [oslo_limit] section
+	    while IFS= read -r line; do
+		key=$(echo "$line" | cut -d'=' -f1 | xargs)
+		value=$(echo "$line" | cut -d'=' -f2- | xargs)
+		
+		# Check if the key exists in the section, if not, add it
+		if grep -A10 "^\[oslo_limit\]" "$config_file" | grep -q "^$key ="; then
+		    echo "Updating $key in oslo_limit section"
+		    sudo sed -i "/^\[oslo_limit\]/,/^\[/ s|^$key =.*|$key = $value|" "$config_file"
+		else
+		    echo "Adding $key to oslo_limit section"
+		    sudo sed -i "/^\[oslo_limit\]/a$key = $value" "$config_file"
+		fi
+	    done <<< "$oslo_limit_conf"
+	    echo "oslo_limit added to [oslo_limit] section"
 
 	else
 	    echo "[oslo_limit] section not found in $config_file"
 	    echo "Adding the [oslo_limit] section and new connection string"
 
     	# If the [oslo_limit] section doesn't exist, append it to the config file
-    	echo -e "\n[oslo_limit]\n$oslo_limit_conf" | sudo tee -a "$config_file"
+    	#echo -e "\n[oslo_limit]\n$oslo_limit_conf" | sudo tee -a "$config_file"
+    	while IFS= read -r line; do
+	   key=$(echo "$line" | cut -d'=' -f1 | xargs)
+	   value=$(echo "$line" | cut -d'=' -f2- | xargs)
+	   # Check if the key exists in the section, if not, add it
+	   if grep -A10 "^\[oslo_limit\]" "$config_file" | grep -q "^$key ="; then
+	       echo "Updating $key in oslo_limit section"
+	       sudo sed -i "/^\[oslo_limit\]/,/^\[/ s|^$key =.*|$key = $value|" "$config_file"
+	   else
+	       echo "Adding $key to oslo_limit section"
+	       sudo sed -i "/^\[oslo_limit\]/a$key = $value" "$config_file"
+	   fi
+	done <<< "$oslo_limit_conf"
+	fi
+	
+	# edit default section
+	if grep -q "^\[DEFAULT\]" "$config_file"; then
+	    echo "[DEFAULT] section found in $config_file"
+
+	    # Add the paste_deploy below the [paste_deploy] section
+	    sudo sed -i "/^\[DEFAULT\]/a$default_conf" "$config_file"
+	    echo "default configuration added to [DEFAULT] section"
+
+	else
+	    echo "[DEFAULT] section not found in $config_file"
+	    echo "Adding the [paste_deploy] section and new connection string"
+
+    	# If the [DEFAULT] section doesn't exist, append it to the config file
+    	echo -e "\n[DEFAULT]\n$default_conf" | sudo tee -a "$config_file"
 	fi
 else
     echo "glance configuration file $config_file not found. Exiting..."
@@ -184,4 +275,3 @@ su -s /bin/sh -c "glance-manage db_sync" glance
 
 # Restart the Image services:
 sudo service glance-api restart
-
